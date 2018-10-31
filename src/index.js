40e76a9bed0a85316802657de37f9bfed8784d7f
coderlfy <846287593@qq.com>
2018-10-31 15:49:09 +0800

    添加react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store'
 import './index.css';
 // import './util/websocket'
@@ -9,6 +10,13 @@ import './index.css';
 import Login from './page/login'
 ReactDOM.render(
 <Provider store={store}>
-	<Login />
+	<div>
+		<BrowserRouter>
+			<Route path='/' render={
+				()=>(<Login />)
+			}>	
+			</Route>
+		</BrowserRouter>
+	</div>
 </Provider>
 , document.getElementById('root'));
